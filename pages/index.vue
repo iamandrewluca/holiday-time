c4a00ee539c1bf801e1bbd310c8711c10057050e
Andrew Luca <thendrluca@gmail.com>
2021-08-04 02:54:55 +0300

    feat(pages): fetch and show 6 activities

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,51 @@
 <template>
   <Container>
-    {{ $options.faker.lorem.paragraphs(20) }}
+    <Row>
+      <Column
+        v-for="item in items"
+        :key="item.id"
+        class="w-full sm:w-1/2 md:w-1/3 mb-10"
+      >
+        <ActivityCard
+          :title="item.title"
+          :description="item.description"
+          :price="item.retail_price.formatted_value"
+          :image="item.cover_image_url"
+        />
+      </Column>
+    </Row>
   </Container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  ref,
+  useContext,
+  useFetch,
+} from '@nuxtjs/composition-api'
 import faker from 'faker'
 import Container from '~/components/container/container.component.vue'
+import { getActivitiesUrl } from '~/utils/get-activities-url'
+import Row from '~/components/row/row.component.vue'
+import Column from '~/components/column/column.component.vue'
+import ActivityCard from '~/components/activity-card/activity-card.component.vue'
 
 export default defineComponent({
   faker,
-  components: { Container },
+  components: { ActivityCard, Column, Row, Container },
+  setup() {
+    const { $axios } = useContext()
+    const items = ref([])
+
+    useFetch(async () => {
+      const response = await $axios.get(getActivitiesUrl(0, 6))
+      items.value = response.data
+    })
+
+    return {
+      items,
+    }
+  },
 })
 </script>
