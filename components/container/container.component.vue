1be47000ca3ed601dc90892dec876cc2ddda3599
Andrew Luca <thendrluca@gmail.com>
2021-08-03 22:04:29 +0300

    feat(components): add grid container (#7)

---
+++ components/container/container.component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+$space: 20px;
+$breakpoints: 640px, 768px, 1024px, 1280px, 1536px;
+
+div {
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: $space;
+  padding-right: $space;
+  width: 100%;
+}
+
+@each $breakpoint in $breakpoints {
+  @media (min-width: #{$breakpoint}) {
+    div {
+      max-width: #{$breakpoint};
+    }
+  }
+}
+</style>
---
+++ components/container/container.stories.ts
@@ -0,0 +1,19 @@
+import { Meta, Story } from '@storybook/vue'
+import { defineComponent } from '@nuxtjs/composition-api'
+import faker from 'faker'
+import Component from './container.component.vue'
+
+export default <Meta>{
+  title: 'Container',
+  component: Component,
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export const Container: Story = () =>
+  defineComponent({
+    components: { Component },
+    faker,
+    template: `<Component>{{ $options.faker.lorem.paragraphs(10) }}</Component>`,
+  })
