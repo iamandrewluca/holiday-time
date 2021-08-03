71f6d5681189e7cb679fec014547f1f6ed917577
Andrew Luca <thendrluca@gmail.com>
2021-08-03 23:21:12 +0300

    feat(components): add basic footer (#10)

---
+++ components/footer/footer.section.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="footer">
+    <Container>
+      <span>✨✨✨</span>
+      <span class="footer__text"
+        >Have fun in {{ new Date().getFullYear() }}</span
+      >
+      <span>✨✨✨</span>
+    </Container>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Container from '~/components/container/container.component.vue'
+
+export default defineComponent({ components: { Container } })
+</script>
+
+<style scoped lang="scss">
+@use '../variables' as variables;
+
+.footer {
+  background-color: variables.$white;
+  color: variables.$goldy;
+  padding-bottom: 1rem;
+  padding-top: 1rem;
+  text-align: center;
+
+  &__text {
+    text-decoration: underline;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="bg-gray-100 min-h-screen">
+  <div class="bg-gray-100 min-h-screen flex flex-col">
     <div class="top-0 sticky">
       <Header />
     </div>
-    <Nuxt />
+    <div class="flex-grow" :class="$style.body">
+      <Nuxt />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import Header from '~/components/header/header.section.vue'
+import Footer from '~/components/footer/footer.section.vue'
 
 export default defineComponent({
-  components: { Header },
+  components: { Header, Footer },
 })
 </script>
+
+<style module lang="scss">
+@use '../components/variables' as variables;
+
+.body {
+  padding-bottom: variables.$gutter;
+  padding-top: variables.$gutter;
+}
+</style>
